a79d15086a5b3228e569d8df8b11847fed6ccaa6
Teng Wu <wutengaaa@163.com>
2023-04-07 16:35:34 +0800

    Build scripts for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((projectile-project-configure-cmd . "cmake -B build -DLLVM_DIR=$(llvm-config --prefix) -DCMAKE_EXPORT_COMPILE_COMMANDS=ON")))
+ (nil . ((projectile-project-compilation-cmd . "make -C build -j4"))))
